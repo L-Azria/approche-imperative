9a199afb12495832a7979410b0000d5cbfb4c2da
L-Azria <l.azria.pro@gmail.com>
2023-05-15 14:24:08 +0200

    AFFICHAGE DE LA VALEUR D’UNE VARIABLE- Retour à la ligne dans une chaine de caractère

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte numberByte = 100;
+        short numberShort = 32760;
+        int numberInt = 150;
+        long numberLong = 55555L;
+        float numberFloat = 112.45F;
+        double numberDouble = 547.00D;
+        char caractere = 'r';
+        boolean a = true;
+        String mot = "Hello";
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+        System.out.println("Variable déclaré :" + numberByte);
+        System.out.println(randomString);
+
+
+    }
+}

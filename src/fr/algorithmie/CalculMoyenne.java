b782fd4abbc3bb87ea78d32948d93a2b37d7fb99
L-Azria <l.azria.pro@gmail.com>
2023-05-19 18:54:59 +0200

    TP3

--- src/fr/algorithmie/CalculMoyenne.java
+++ src/fr/algorithmie/CalculMoyenne.java
@@ -3,11 +3,11 @@ package fr.algorithmie;
 public class CalculMoyenne {
     public static void main(String[] args) {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-        int somme = 0;
+        float somme = 0;
         for(int i = 0; i< array.length; i++){
             somme= somme+array[i];
         }
-        int moyenne = somme/ array.length;
+        float moyenne = somme/ array.length;
         System.out.println("La moyenne est : " + moyenne );
     }
 }
--- src/fr/algorithmie/Rotation.java
+++ src/fr/algorithmie/Rotation.java
@@ -2,7 +2,15 @@ package fr.algorithmie;
 
 public class Rotation {
     public static void main(String[] args) {
-        int[] tableauEntiers = {1,2,3,4,1};
+        int[] tableauEntiers = {1,2,3,4,5};
+        int[] rotation = new int[6];
+        int i;
+        System.out.println("rotation à droite :");
+        rotation[0]=tableauEntiers[tableauEntiers.length-1];
+        for(i=0; i< tableauEntiers.length; i++){
+            rotation[i+1] = tableauEntiers[i];
+            System.out.println(rotation[i]);
+        }
 
     }
 }
